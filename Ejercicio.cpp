a0d8e2b37b0c18d25cbee5a6e1fbb9427cbdcea8
Jabril Hassan Álvarez <47892513+jshassanalvarez@users.noreply.github.com>
2019-10-02 10:03:48 +0200

    Update Ejercicio.cpp

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void readVector(int *v, int n){
+void readVector(int n){
 	for(int i=0;i<=n;i++){
 		cout<<"v["<<i<<"]= "<<endl;
 		cin>>v[i];
@@ -10,7 +10,7 @@ void readVector(int *v, int n){
 }
 
 
-void showVectores(int *v,int n){
+void showVectores(int n){
 	for(int i=0;i<=n;i++){
 		cout<<v[i];
 	}
